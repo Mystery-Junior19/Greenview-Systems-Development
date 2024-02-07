a19e1208f9b70a238f47531e76f473a012802897
Mystery-Junior19 <hdicksonjunior@gmail.com>
2024-02-07 12:30:55 +0200

    First Git Class Test

--- Python/Pycode-1.py
+++ Python/Pycode-1.py
@@ -1,8 +1,7 @@
 class Car:
     """ Represents a car in a car lot """
-    def ..init..(Self, make, model, year, Price_usd):
+    def __init__(Self, make, model, year, Price_usd):
         Self.make = make
         Self.model = model
         Self.year = year
         Self.Price_usd = Price_usd
-
